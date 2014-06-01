0ccbf1ca6df125e571b93b1db338d87dcfebff3f
Bartosz Nowak <sztosz@gmail.com>
2014-06-01 19:04:27 +0200

    Home page now does accept POST requests and shows what has been posted

--- functional_tests.py
+++ functional_tests.py
@@ -23,28 +23,31 @@ class NewVisitorTest(unittest.TestCase):
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        expected_text = '1: Buy peacock feathers'
+        self.assertIn(expected_text, [row.text for row in rows])
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peackok feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        expected_text = '2: Use peacock feathers to make a fly'
+        self.assertIn(expected_text, [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
--- lists/tests.py
+++ lists/tests.py
@@ -10,10 +10,26 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
+
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
+
         response = home_page(request)
         expected_html = render_to_string('home.html')
+
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_post_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'},
+        )
         self.assertEqual(response.content.decode(), expected_html)
